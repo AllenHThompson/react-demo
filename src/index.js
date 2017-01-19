add1e81408e07471c3dd9994cc6bc5e718d5e1f9
Allen H. Thompson <allenhthompson1@gmail.com>
2017-01-19 17:25:35 -0500

    Deleted reference to sandbox file.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import App_lesson_8 from './App_lesson_8';
 
 ReactDOM.render(
-  <App_lesson_8 cat={5}  />,
+  <App />,
+  // eslint-disable-next-line
   document.getElementById('root')
 );
