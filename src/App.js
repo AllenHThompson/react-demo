1e4e58615d781c8f7495ac15c93ada102b78eec6
Allen H. Thompson <allenhthompson1@gmail.com>
2017-01-19 17:25:20 -0500

    Working on different lessons in same file.

--- src/App.js
+++ src/App.js
@@ -1,45 +1,21 @@
-// //<project directory> npm install --save-dev eslint-config-rallycoding
-// //include .eslintrc { "extends": "rallycoding"}
-//
-// import React from 'react';
-//
-// //'class component' can have state
-// class App extends React.Component {
-//   constructor() {
-//     //SUPER GIVES 'THIS' THE CONTEXT W/IN THE COMPONENT INSTEAD OF PARENT CLASS
-//     super();
-//     this.state = {
-//       txt: 'this is the state text',
-//       cat: 0
-//     };
-//   }
-//
-//   update(e) {
-//     this.setState({txt: e.target.value})
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         <h1>{this.state.txt}</h1>
-//         <Widget update={this.update.bind(this)} />
-//
-//       </div>
-//     );
-//   }
-// }
-// //'stateless function component' can't have state
-// // const App = () => <h1>Hello Stateless</h1>
-//
-// // App.propTypes = {
-// //   txt: React.PropTypes.string,
-// //   cat: React.PropTypes.number.isRequired
-// // };
-// //
-// // App.defaultProps = {
-// //   txt: 'this is the default text'
-// // };
-//
-// const Widget = (props) =>
-//   <input type="text" onChange={props.update}/>
-// export default App;
+//CD to project direcorty and type 'npm start' to start react server
+//<project directory> npm install --save-dev eslint-config-rallycoding
+//include .eslintrc { "extends": "rallycoding"}
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return <Button>I <Heart />React</Button>;
+  }
+}
+
+const Button = (props) => <button>{props.children}</button>;
+
+// eslint-disable-next-line
+class Heart extends React.Component {
+  render() {
+    return <span>&hearts;</span>;
+  }
+}
+
+export default App;
