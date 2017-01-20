98618192b5fe8b5bb34cace05f08931fc69194c8
Allen H. Thompson <allenhthompson1@gmail.com>
2017-01-20 15:54:54 -0500

    Added wrapper component to mount and unmount.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,59 @@
 //<project directory> npm install --save-dev eslint-config-rallycoding
 //include .eslintrc { "extends": "rallycoding"}
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { val: 0 };
+    this.update = this.update.bind(this);
+  }
+
+  update() {
+    this.setState({ val: this.state.val + 1 });
+  }
+
+  componentWillMount() {
+    console.log('componetWillMount');
+    this.setState({ m: 2 });
+  }
+
   render() {
-    return <Button>I <Heart />React</Button>;
+    console.log('render');
+    return (<button onClick={this.update}>
+      {this.state.val * this.state.m}
+    </button>
+    );
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    this.inc = setInterval(this.update, 500);
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+    clearInterval(this.inc)
   }
 }
 
-const Button = (props) => <button>{props.children}</button>;
-
-// eslint-disable-next-line
-class Heart extends React.Component {
+class Wrapper extends React.Component {
+  mount() {
+    ReactDOM.render(<App />, document.getElementById('a'));
+  }
+  unmount() {
+    ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+  }
   render() {
-    return <span>&hearts;</span>;
+    return (
+      <div>
+        <button onClick={this.mount.bind(this)}>Mount</button>
+        <button onClick={this.unmount.bind(this)}>UnMount</button>
+        <div id="a"></div>
+      </div>
+    );
   }
 }
 
-export default App;
+export default Wrapper;
